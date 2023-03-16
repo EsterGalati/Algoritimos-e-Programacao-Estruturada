09c5281769728a260818e86f6ec8e18c7bd6f4dd
EsterGalati <esterscgalati@gmail.com>
2023-03-16 18:18:41 -0300

    Atividade 1

---
+++ Atividade 1/Ex6.c
@@ -0,0 +1,24 @@
+/*Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste.
+Calcular e escrever o valor do novo salário.*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    float salario, reajuste, desconto, novosalario;
+
+    printf("Digite o seu salario:");
+    scanf("%f", &salario);
+    printf("Digite o valor em porcentagem do reajuste:");
+    scanf("%f", &reajuste);
+
+    desconto=(reajuste * salario) / 100;
+    novosalario=(salario - desconto);
+
+    printf("Seu novo salario e %2.f:",novosalario);
+
+
+    return 0;
+}
