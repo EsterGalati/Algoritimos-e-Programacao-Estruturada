4a9dac1be035bed3ddee1b3ea64209219b591c54
Ester <esterscgalati@gmail.com>
2023-04-17 20:01:11 -0300

    Exercicios

---
+++ Exercicios em Sala.c/1.c
@@ -0,0 +1,21 @@
+/*VETORES*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    float estudante_1, estudante_2, estudante_3, estudante_4, estudante_5, soma;
+
+    estudante_1 = 10;
+    estudante_2 = 9;
+    estudante_3 = 8;
+    estudante_4 = 7;
+    estudante_5 = 10;
+
+    soma = estudante_1 + estudante_2 + estudante_3 + estudante_4 + estudante_5;
+
+    printf("Media estutandes: %.2f\n", soma / 5);
+
+    return 0;
+}
